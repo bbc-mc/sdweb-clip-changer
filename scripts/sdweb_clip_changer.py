ca4f38b0c0331e7439d3b837e9b1eff41c638d36
bbc_mc <bbc_mc@hotmail.co.jp>
2023-01-22 21:00:00 +0900

    Update: correctly work with official LoRA built-in extension

--- scripts/sdweb_clip_changer.py
+++ scripts/sdweb_clip_changer.py
@@ -3,6 +3,7 @@
 
 import torch
 import ldm
+import os
 
 from modules import sd_models
 from modules import paths, script_callbacks, shared
@@ -121,6 +122,37 @@ def on_model_loaded(sd_model):
         return
 
     hijack_hijack(sd_model)
+
+def on_app_started(demo, app):
+
+    list_must_be_before_them = ["lora_script.py"]
+
+    callbacks_model_loaded = script_callbacks.callback_map.get("callbacks_model_loaded", None)
+
+    if callbacks_model_loaded is None:
+        return
+
+    def find_script(name):
+        for i, callback in enumerate(callbacks_model_loaded):
+            callback:script_callbacks.ScriptCallback
+            if os.path.basename(callback.script) == name:
+                return i
+        return None
+
+    if len(callbacks_model_loaded) > 0:
+        for index, callback in enumerate(callbacks_model_loaded):
+            for file_before in list_must_be_before_them:
+                callback:script_callbacks.ScriptCallback
+                my_index = find_script("sdweb_clip_changer.py")
+                if os.path.basename(callback.script) == file_before and my_index > index:
+                    me = callbacks_model_loaded.pop(my_index)
+                    callbacks_model_loaded.insert(index, me)
+                    print(f"found {os.path.basename(callback.script)}. exchange my callback index {my_index} -> {index}")
+
+    print("CLIP Changer.on_app_started done.")
+
+#
+script_callbacks.on_app_started(on_app_started)
 
 #
 script_callbacks.on_model_loaded(on_model_loaded)
